d507e0800452b3bd5402bbb91107af5bee60b2be
zhangxu <zhangxu_wo@163.com>
2017-08-11 23:59:00 +0800

    update

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const pify = require('pify');
 let sitpage = null;
 let phInstance = null;
 let windowH = 0;
+let arr = [{
+	"dom": "#lj_car",
+	"urlkeyword": "LJcore;public/LJselector;public/LJajax"
+}, {
+	"dom": "#t02",
+	"urlkeyword": "getFmtVideoNew"
+}];
+
+let scrollPage = function(page, dom, keyword) {
+	console.log(dom, keyword)
+	return page.evaluate(function(ele) {
+		return document.querySelector(ele).offsetTop;
+	}, dom).then(function(top) {
+		console.log(top)
+		let script1 = "function(){ window.scrollTo(0, 500); }";
+		return page.evaluateJavaScript(script1);
+	});
+};
 
 phantom.create()
 	.then(instance => {
@@ -16,40 +34,50 @@ phantom.create()
 	})
 	.then(status => {
 		console.log('status ' + status);
+		sitpage.on('onConsoleMessage', function(msg, lineNum, sourceId) {
+			console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+		});
 		return sitpage.evaluate(function() {
-			window.scrollTo(0, 20000);
 			return document.getElementsByTagName('html')[0].getBoundingClientRect().height;
 		});
 	})
 	.then(height => {
 		console.log('page height: ' + height);
 		windowH = height;
-		phInstance.exit();
-		phInstance = null;
-		sitpage = null;
-		return phantom.create();
-	})
-	.then(instance => {
-		phInstance = instance;
+		sitpage.close();
 		return phInstance.createPage();
 	})
 	.then(page => {
 		sitpage = page;
+		sitpage.on('onResourceRequested', function(requestData) {
+			let match = requestData.url.match(/(getFmtVideoNew)/g);
+			if (match) {
+				console.log('Request (#' + requestData.url + ')');
+				setTimeout(function() {
+					page.render('./output/4 ' + arr.length + '.jpg');
+					arr.splice(0, 1);
+					if (!arr.length) {
+						phInstance.exit();
+					}
+				}, 2000);
+			}
+		});
 		return sitpage.property('viewportSize', {
 			width: 1200,
 			height: windowH
 		});
 	})
-	.then(p => {
-		console.log('p: ' + p);
+	.then(_ => {
 		return sitpage.open('http://bj.leju.com');
 	})
 	.then(status => {
-		console.log('status: '+ status);
-		setTimeout(function() {
-			sitpage.render('./output/3.jpg');
-			phInstance.exit();
-		}, 2000);
+		console.log('status: ' + status);
+		Promise.all(arr.forEach(function(item) {
+			scrollPage(sitpage, item.dom, item.urlkeyword);
+		}));
+	})
+	.then(k => {
+		console.log(k)
 	})
 	.catch(error => {
 		console.log(error);
